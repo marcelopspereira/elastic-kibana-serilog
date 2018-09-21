caef4b1712941492bc9ab3b2bfd5a4ee21940d61
Marcelo Pereira da Silva <mps@mbmseguros.com.br>
2018-09-21 16:58:22 -0300

    add implementaçao serilog em controller e startup

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -3,41 +3,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using elasctic_kibana.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-namespace elasctic_kibana.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace elasctic_kibana.Controllers {
+    public class HomeController : Controller {
+        ILogger<HomeController> _logger;
+        public HomeController (ILogger<HomeController> logger) {
+            _logger = logger;
         }
 
-        public IActionResult About()
-        {
+        public IActionResult Index () {
+            _logger.LogInformation ($"oh hai there! : {DateTime.UtcNow}");
+            try {
+                throw new Exception ("oops. i haz cause error in UR codez.");
+            } catch (Exception ex) {
+                _logger.LogError (ex, "ur code iz buggy.");
+            }
+            return View ();
+        }
+
+        public IActionResult About () {
             ViewData["Message"] = "Your application description page.";
 
-            return View();
+            return View ();
         }
 
-        public IActionResult Contact()
-        {
+        public IActionResult Contact () {
             ViewData["Message"] = "Your contact page.";
 
-            return View();
+            return View ();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Privacy () {
+            return View ();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -9,52 +9,54 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
-namespace elasctic_kibana
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace elasctic_kibana {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
+
+            var elasticUri = Configuration["ElasticConfiguration:Uri"];
+
+            Log.Logger = new LoggerConfiguration ()
+                .Enrich.FromLogContext ()
+                .WriteTo.Elasticsearch (new ElasticsearchSinkOptions (new Uri (elasticUri)) {
+                    AutoRegisterTemplate = true,
+                })
+                .CreateLogger ();
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.Configure<CookiePolicyOptions> (options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
+                app.UseHsts ();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            loggerFactory.AddSerilog ();
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
+            app.UseCookiePolicy ();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
